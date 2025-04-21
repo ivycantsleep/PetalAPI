0a551ba46dd15023dd8d688f1ac4aa9b3f7d386f
Sapphire <54ph@is-likely.gay>
2025-04-21 15:25:30 +0200

    [Feat] Build system: Early draft of the build scripts

---
+++ build/scripts/i386-build_only_crt.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+title PetalAPI Builder
+
+cls
+
+echo:
+echo: PetalAPI Build Script - PetalCRT
+echo: _______________________________________________________
+echo:
+echo: This hasn't been implemented yet.
+echo:
+echo: Press any key to exit.
+pause >nul
---
+++ build/scripts/i386-build_only_drivers.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+title PetalAPI Builder
+
+cls
+
+echo:
+echo: PetalAPI Build Script - Drivers
+echo: _______________________________________________________
+echo:
+echo: This hasn't been implemented yet.
+echo:
+echo: Press any key to exit.
+pause >nul
