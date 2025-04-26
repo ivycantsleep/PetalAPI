31c39ac5868555621156704f1404003f2e95a350
Sapphire <54ph@is-likely.gay>
2025-04-26 16:28:37 +0200

    [Misc] Repository rebase and restructuring, now based on NT 5.2 (Windows Server 2003) source code

--- src/dlls/kernel32/_security.c
+++
@@ -1,9 +0,0 @@
-#include <basedll.h>
-
-//from Wine seucurity.c
-
-BOOL set_ntstatus(NTSTATUS status)
-{
-    if (!NT_SUCCESS(status)) SetLastError( RtlNtStatusToDosError( status ));
-    return NT_SUCCESS(status);
-}
--- src/dlls/kernel32/_utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_EXTRA_H // [
-
-#define UTILS_EXTRA_H
-
-//from Wine utils.h
-
-#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
-
-#endif
